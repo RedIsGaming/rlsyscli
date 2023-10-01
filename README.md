f5fd74e1830bc94d33ea33fb5c780b3ba5d0d1e8
RedIsGaming <jaredhofman@protonmail.com>
2023-10-01 11:01:35 +0100

    docs/fix: Updated the README.md and support for Windows

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 # rlsyscli
-A functional CLI to show your system info.
-This application is written in C#. A 
-utiliy tool to show your disk usage as
-well as your CPU/RAM and process usage.
-<br/>
-
-Still in development...
+A functional CLI to show your system info. This application is written in C# and Shell.<br/> 
+A utility tool to show your disk usage as well as your CPU/RAM and process usage.<br/>
 
 ## Author
 > RedIsGaming
@@ -14,15 +9,32 @@ Still in development...
 > Apache-2.0 license
 
 ## How to use this project?
+You can clone this project anywhere on your PC. 
+To make this project work, follow the steps below: <br/>
 ```shell
+#step 1
+git clone https://github.com/RedIsGaming/rlsyscli.git
+OR
 git clone git@github.com:RedIsGaming/rlsyscli.git
+
+#step 2
 cd rlsyscli
-//commands will be added later.
+
+#step 3
+source "path/rlsyscli/disk.sh" #Where path is the location of your directory.
+
+#step 4
+sysdisk
 ```
 
 ## Status
 > On going
 
+## Tasks
+- [x] Disk display Linux/Windows.
+- [ ] CPU/RAM usage display.
+- [ ] Process usage display.
+
 ## Contact
-> Idea's or want to help? `shoot a 
-message in Discord: redisgaming`
+> Want to help or want to message me?
+> Shoot a message in Discord: `redisgaming` for contact.
--- disk.sh
+++ disk.sh
@@ -1,39 +1,37 @@
 function sysdisk() {
     local path;
-    path="$(dirname -- "$PWD")";
-    local executable="$path/rlsyscli/disk.sh";
     local result="$?";
+    
+    if [ "$OSTYPE" == "linux-gnu" ]
+        then path="$(dirname -- "$PWD")";
+    fi;
+    
+    if [ "$OSTYPE" == "msys" ]
+        then path="$(pwd -W)/";
+    fi;
+    
+    local executable="$path/rlsyscli/disk.sh";
     cd "$path/rlsyscli/rlsyscli" || return 1;
     
     for file in *.csproj;
         do if [ -f "$file" ];
             then dotnet run;
             cd ..;
+            return $result;
             
-            if [ "$result" -eq 0 ]
-                then return 0;
-                
-            else
-                echo "There was an error with reading the .csproj file!";
-                return 1;
-            fi      
         else
             echo "The directory doesn't contain a valid .csproj file!";
+            return 1;
         fi;
     done;
 
     if [ -f "$executable" ];
         then chmod +x "$executable";
         ./"$executable";
-        
-        if [ "$result" -eq 0 ]
-            then return 0;
-            
-        else
-            echo "There was an error with reading the disk.sh file!";
-            return 1;
-        fi         
+        return $result;
+      
     else
         echo "The directory doesn't contain a valid disk.sh file!";
+        return 1;
     fi;
 }
